44d2d740605f7488bef4687efc13f4ea4f0145a3
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-02-08 16:06:09 +0100

    password generated in redirect

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
-$seed = str_split('abcdefghijklmnopqrstuvwxyz'.'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.'0123456789!@#$%^&*()');
-shuffle($seed);
-$rand = '';
-foreach (array_rand($seed, 10) as $i) $rand .= $seed[$i];
-echo $rand
+// $seed = str_split('abcdefghijklmnopqrstuvwxyz'.'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.'0123456789!@#$%^&*()');
+// shuffle($seed);
+// $rand = '';
+// foreach (array_rand($seed, 10) as $i) $rand .= $seed[$i];
+// echo $rand
+
+if(isset($_GET['number'])){
+    $numero = $_GET['number'];
+}else {
+    $numero = '3';
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -18,10 +25,37 @@ echo $rand
 </head>
 <body>
     <div class="container text-center">
-        <h1>Password generator super ganzo</h1>
-        <h2>Creiamo un password tutta per te!</h2>
+        <div class="app-wrapper">
 
-        <form action="" method="GET" name="number"></form>
+            <h1>Password generator super ganzo</h1>
+            <h2>Creiamo un password tutta per te!</h2>
+            
+            <div class="main-wrapper d-flex justify-content-between p-5">
+                <p>Seleziona una lungezza per la tua password:</p>
+                
+                <form action="index.php" method="GET">
+                    <input type="number" class="form-control" min='4' max='15' name="number" value="<?= $numero ?>" >
+                </form>
+
+                <?php if(isset($_GET['number'])) : ?>
+                    <p>
+                    la password :
+                        <?php 
+                            session_start();
+                            $root = str_split('abcdefghijklmnopqrstuvwxyz'.'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.'0123456789@()#$%!&*');
+                            shuffle($root);
+                            $random= '';
+                            foreach(array_rand($root, $numero) as $i) $random .= $root[$i];
+                            $_SESSION['password'] = $random;
+                            header('Location: components/result.php')
+                        ?>
+                    </p>
+                <?php endif; ?>
+            </div>
+                <?php 
+                    var_dump($numero)
+                ?>
+        </div>
     </div>
 </body>
 </html>
